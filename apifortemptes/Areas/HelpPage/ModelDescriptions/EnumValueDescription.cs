9228aad069c0b6155b7c324b3401c9850f1d18b3
Ettie1 <efverwant@gmail.com>
2024-07-08 13:47:17 -0800

    first commit

---
+++ apifortemptes/Areas/HelpPage/ModelDescriptions/EnumValueDescription.cs
@@ -0,0 +1,11 @@
+namespace apifortemptes.Areas.HelpPage.ModelDescriptions
+{
+    public class EnumValueDescription
+    {
+        public string Documentation { get; set; }
+
+        public string Name { get; set; }
+
+        public string Value { get; set; }
+    }
+}
